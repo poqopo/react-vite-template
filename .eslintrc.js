3b4a865688f34c2da9d070c691d343a69bd4eff1
2namkyung <iddbxk@daum.net>
2023-02-18 11:39:43 +0900

    vite version upgrade, eslint 정리

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,5 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: false,
-    },
-    ecmaVersion: 13,
-    sourceType: 'module',
-  },
   plugins: ['react', '@typescript-eslint'],
   extends: [
     'eslint:recommended',
@@ -24,8 +17,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    'react/jsx-filename-extension': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
     'react/react-in-jsx-scope': 'off',
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
 import './styles/index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
 
 root.render(<App />);
