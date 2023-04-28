e5d591b1f676ea8b1ce23dea25aae6f363013c7d
namkyung.lee <iddbxk@daum.net>
2023-04-28 19:53:55 +0900

    jest moduleNameMapper config 변경: awesome vite 참조

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,12 @@ module.exports = {
     '!**/node_modules/**',
     '!**/*.d.ts',
   ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^.+\\.module\\.(css|sass|scss|less)$': 'identity-obj-proxy',
+    '^.+\\.(css|sass|scss|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg|ttf|woff|woff2)$': `<rootDir>/__mocks__/fileMock.js`,
+  },
   setupFilesAfterEnv: ['<rootDir>/setupTest.ts'],
   verbose: true,
   testTimeout: 30000,
